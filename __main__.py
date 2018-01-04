5aa0ae24b912ea0fc1366b3f3415bd86e1bc8dc8
volaxar <volaxar@gmail.com>
2018-01-04 16:36:58 +0500

    Инициализировали точку при создании

--- __main__.py
+++ __main__.py
@@ -6,18 +6,10 @@ from point import Point
 terminal.open()
 terminal.set('input.filter=[keyboard, close]')
 
-p1 = Point()
-p1.x = 1
-p1.y = 3
-p1.sym = '*'
-
+p1 = Point(1, 3, '*')
 p1.draw(terminal)
 
-p2 = Point()
-p2.x = 4
-p2.y = 5
-p2.sym = '#'
-
+p2 = Point(4, 5, '#')
 p2.draw(terminal)
 
 terminal.refresh()
