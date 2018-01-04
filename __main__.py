e61ee96790ce54b9af3c1f0eb00afd8cfaad8c5d
volaxar <volaxar@gmail.com>
2018-01-04 13:26:51 +0500

    Hello world

---
+++ __main__.py
@@ -0,0 +1,11 @@
+from bearlibterminal import terminal
+
+terminal.open()
+
+terminal.printf(0, 0, 'Hello, world!')
+terminal.refresh()
+
+while terminal.read() != terminal.TK_CLOSE:
+    pass
+
+terminal.close()
