5a590da8cff987a8e55b80fffe60fcdc171dc110
volaxar <volaxar@gmail.com>
2018-01-04 14:22:52 +0500

    Процедурный подход. Функция Draw

--- __main__.py
+++ __main__.py
@@ -1,8 +1,24 @@
 from bearlibterminal import terminal
 
+
+def draw(x, y, sym):
+    terminal.put(x, y, sym)
+
+
 terminal.open()
 
-terminal.printf(0, 0, 'Hello, world!')
+x1 = 1
+y1 = 3
+sym1 = '*'
+
+draw(x1, y1, sym1)
+
+x2 = 4
+y2 = 5
+sym2 = '#'
+
+draw(x2, y2, sym2)
+
 terminal.refresh()
 
 while terminal.read() != terminal.TK_CLOSE:
